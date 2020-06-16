ba8c280eaf9166d69df9a0d0f2fb700458a7146f
drizzer14 <drizzer14@gmail.com>
2020-06-16 14:36:02 +0300

    feat!: Replace `useRequest` with `useAsync` hook with changed API

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export { default as useRequest } from './use-request';
+export { default as useAsync } from './use-async';
 export { default as useMount } from './use-mount';
 export { default as useAutoTransition } from './use-auto-transition';
